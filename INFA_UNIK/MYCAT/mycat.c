dcb3e3dc509281e27c96e5d56927f92334fd4dfd
GELGOOG-STUTZER <anton.okley@mail.ru>
2020-09-21 00:52:03 +0300

    Home assignement done

---
+++ INFA_UNIK/MYCAT/mycat.c
@@ -0,0 +1,20 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main( int argc, char *argv[] )
+{
+	int i, fd, ch;
+
+	for (i = 1; i < argc; i++)
+		{
+		fd = open( argv[i], O_RDONLY );
+		if (fd < 0) {
+			printf("open error %s\n", argv[i]);
+			continue;
+		}
+		while (read(fd, &ch, 1) == 1) 
+			write(STDOUT_FILENO, &ch, 1);
+		close(fd);
+		}
+}
