556a5b8a71f165c4a9f28fb2c9bf8335921972bc
GELGOOG-STUTZER <anton.okley@mail.ru>
2020-12-12 16:24:29 +0300

    3

--- INFA_UNIK/FINAL/proc.c
+++ INFA_UNIK/FINAL/proc.c
@@ -1,16 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
 #include <grp.h>
 #include <dirent.h>
 
+void dir_count(char* path,int* count) {
+     char new_path[1000];
+     struct dirent *current;
+     DIR *dir = opendir(path);
+
+     if(!dir) {
+        return;
+     }
+
+     while((current = readdir(dir)) != NULL) {
+        if((strcmp(current->d_name, "." ) != 0) && (strcmp(current->d_name, "..") != 0)) {
+            
+            *count = *count + 1;
+            printf("Count = %d\n", *count);
+            strcpy(new_path, path);
+            strcat(new_path, "/");
+            strcat(new_path, current->d_name);
+            dir_count(new_path, count);
+        }
+    }
+    closedir(dir);
+    return;
+}
+
 
 int main (int argc, char* argv[]) {
 	struct dirent *current;
     DIR *dir;
+    int total_count = 0;
     int proc_count = 0;
     dir = opendir("/proc");
     current = readdir(dir);
@@ -19,6 +44,9 @@ int main (int argc, char* argv[]) {
        proc_count++; 
     current = readdir(dir);
     }
+    closedir(dir);
+    dir_count("/proc", &total_count);
     printf("Number of processes: %d\n", proc_count);
+    printf("Total count: %d\n", total_count);
 }
 
